a13825a360ed112ff15299f40633ff1d292a6517
Adnan Islam <95557642+AD4NAN@users.noreply.github.com>
2022-01-14 00:37:06 +0600

    Add files via upload

---
+++ dorks-Finder.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+# A notice to all nerds and n00bs...
+# If you will copy the developer's work it will not make you a hacker..!
+# Respect all developers, we doing this because it's fun...
+# So please, don't copy this code.
+
+
+import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,cookielib,requests,mechanize
+os.system('clear')
+print """
+\033[93mooooooooooooooooooooooooooooooooooooooooooooo
+\033[1;91m  
+.d88888b   .d888888  dP     dP dP  888888ba  
+88.    "' d8'    88  88   .d8' 88  88    `8b 
+`Y88888b. 88aaaaa88a 88aaa8P'  88 a88aaaa8P' 
+      `8b 88     88  88   `8b. 88  88   `8b. 
+d8'   .8P 88     88  88     88 88  88    .88 
+Y88888P  88     88  dP     dP dP  88888888P 
+
+\033[93mooooooooooooooooooooooooooooooooooooooooooooo
+
+"""
+
+
+print("Created By:-> \033[1;92mSAK1B \033[90m& \033[94mAD4NAN")
+time.sleep(3)
+print ""
+print ""
+
+CorrectUsername = "SAK1B"
+CorrectPassword = "AD4NAN"
+
+loop = 'true'
+while (loop == 'true'):
+    username = raw_input("Tool Username : ")
+    if (username == CorrectUsername):
+    	password = raw_input("Tool Password : ")
+        if (password == CorrectPassword):
+            print "Logged in successfully as " + username
+            os.system('xdg-open https://www.facebook.com/Adnan.Islam.0.2')
+            
+	    time.sleep(5)
+            loop = 'false'
+        else:
+            print "\033[1;93mWrong Password"
+            os.system('xdg-open https://www.facebook.com/Adnan.Islam.0.2')
+    else:
+        print "\033[1;94mWrong Username"
+        os.system('xdg-open https://www.facebook.com/Adnan.Islam.0.2')
+        
+        
+os.system('clear')
+      
+        
+os.system ('python3 ...')
